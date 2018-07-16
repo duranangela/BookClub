15bbb40481ebc460d2f277abf4a843fe16a633db
Angela Duran <duranangela19@gmail.com>
2018-07-16 17:44:56 -0600

    Add new and create action, view and form partial for author

--- spec/features/authors/user_can_create_author_spec.rb
+++ spec/features/authors/user_can_create_author_spec.rb
@@ -6,7 +6,7 @@ describe 'user can create author' do
     author = 'Orson Scott Card'
     visit new_author_path
 
-    fill_in :author_name, with: 'author'
+    fill_in :author_name, with: author
     click_on "Create Author"
 
     expect(current_path).to eq(author_path(Author.last))
