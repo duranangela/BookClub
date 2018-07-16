0c2da5b516f165d82958cdf88fb21da38580c11c
Angela Duran <duranangela19@gmail.com>
2018-07-16 17:35:36 -0600

    Update test and view to add links from author index page to show page

--- spec/features/authors/user_sees_all_authors_spec.rb
+++ spec/features/authors/user_sees_all_authors_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'user visits author index page' do
-  it 'sees list of all authors' do
+  it 'sees list of all authors, with links to show page' do
     author1 = Author.create(name: 'Orson Scott Card')
     author2 = Author.create(name: 'Sherri S. Tepper')
     author3 = Author.create(name: 'Stephen King')
@@ -11,5 +11,9 @@ describe 'user visits author index page' do
     expect(page).to have_content(author1.name)
     expect(page).to have_content(author2.name)
     expect(page).to have_content(author3.name)
+
+    click_on author1.name
+
+    expect(current_path).to eq(author_path(author1))
   end
 end
