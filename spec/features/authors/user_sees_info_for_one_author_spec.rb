a0de79e0b9022031bb179be7e9adb3808dd7a456
Angela Duran <duranangela19@gmail.com>
2018-07-16 19:13:59 -0600

    Update author index test, add links, update seed file

--- spec/features/authors/user_sees_info_for_one_author_spec.rb
+++ spec/features/authors/user_sees_info_for_one_author_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 describe 'user sees info on author show page' do
   it 'sees author name and books by that author' do
     author1 = Author.create(name: 'Orson Scott Card')
+    book1 = author1.books.create(title: "Ender's Game", year: 1985, image: "Ender's_game.jpg")
+    book2 = author1.books.create(title: "Treason", year: 1988, image: "Treason.jpg")
 
     visit author_path(author1)
 
     expect(page).to have_content(author1.name)
+    expect(page).to have_content(book1.title)
+    expect(page).to have_content(book2.title)
+
   end
 end
