a8c00f13eeb3cf7cbddf094ea3dec811fd26f13c
Angela Duran <duranangela19@gmail.com>
2018-07-17 13:47:46 -0600

    Add sessions controller, new and create actions for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   resources :authors do
     resources :books, except: [:index]
