61dd16e9adbce8c7eb83ebff9776536d49f8de98
Angela Duran <duranangela19@gmail.com>
2018-07-17 11:18:15 -0600

    Add welcome and users stuff, users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
   resources :authors do
     resources :books, except: [:index]
   end
   resources :books, only: [:index]
 
+  resources :users, only: [:new]
 end
