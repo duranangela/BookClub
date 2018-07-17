a5f1a7c0e03f6ea7a422dfa6b72e7e3e342bbf94
Angela Duran <duranangela19@gmail.com>
2018-07-17 10:59:51 -0600

    Add feature test for user registration

---
+++ spec/features/users/registration_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'registration workflow' do
+  # before(:each) do
+  #   User.create(username: 'bob', password: 'password')
+  # end
+
+  it 'submits registration form successfully' do
+    username = 'funtime'
+
+    visit '/'
+
+    click_on 'Sign up to be a New User'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in :user_username, with: username
+    fill_in :user_password, with: '12345'
+
+    click_on 'Create User'
+
+    expect(current_path).to eq(user_path(User.last))
+    expect(page).to have_content("Welcome, #{username}")
+  end
+end
