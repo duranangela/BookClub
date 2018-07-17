6a5ded4958a5b199785d41072afff38a221679db
Angela Duran <duranangela19@gmail.com>
2018-07-16 18:47:31 -0600

    Add relationships in book and author models

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
 
-  validates_presence_of :name
+  has_many :books, dependent: :destroy
 
+  validates_presence_of :name
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
 
+  belongs_to :author
+
   validates_presence_of :title, :year, :image
 
 end
