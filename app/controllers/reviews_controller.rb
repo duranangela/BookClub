a116d22ef0c8b8d6f4473555430310609576505c
Angela Duran <duranangela19@gmail.com>
2018-07-18 07:43:47 -0600

    Add edit and update actions for review, update views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,6 +15,22 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @review = Review.find(params[:id])
+    @book = Book.find(params[:book_id])
+  end
+
+  def update
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.find(params[:id])
+    @review.update(review_params)
+    if @review.save
+      redirect_to author_book_path(@book.author, @book)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def review_params
--- spec/features/reviews/edit_reviews_spec.rb
+++ spec/features/reviews/edit_reviews_spec.rb
@@ -19,7 +19,43 @@ describe 'edit reviews' do
       expect(current_path).to eq(edit_book_review_path(book, review1))
       fill_in :review_rating, with: rating
       fill_in :review_comment, with: comment
+      click_on 'Update Review'
+
+      expect(current_path).to eq(author_book_path(author, book))
+      expect(page).to have_content(rating)
+      expect(page).to have_content(comment)
+    end
+  end
+  context 'as user' do
+    it 'cannot see edit link on book show page' do
+      user = User.create(username: 'Angela', password: '12345')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      author = Author.create(name: 'Card, Orson Scott')
+      book = author.books.create(title: "Ender's Game", year: 1985, image: "Ender's_game.jpg")
+      review1 = user.reviews.create(rating: 5, comment: 'Excellent!', book_id: book.id)
+
+      visit author_book_path(author, book)
+      expect(page).to_not have_content('Edit Review')
+    end
+    it 'can edit reviews from user show page' do
+      user = User.create(username: 'Angela', password: '12345')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      author = Author.create(name: 'Card, Orson Scott')
+      book = author.books.create(title: "Ender's Game", year: 1985, image: "Ender's_game.jpg")
+      review1 = user.reviews.create(rating: 5, comment: 'Excellent!', book_id: book.id)
+
+      rating = 4
+      comment = 'Pretty good'
+
+      visit user_path(user)
       click_on 'Edit Review'
+
+      expect(current_path).to eq(edit_book_review_path(book, review1))
+      fill_in :review_rating, with: rating
+      fill_in :review_comment, with: comment
+      click_on 'Update Review'
 
       expect(current_path).to eq(author_book_path(author, book))
       expect(page).to have_content(rating)
