36054c2edd2edc1c2989263f31eab7c3a7eed257
Angela Duran <duranangela19@gmail.com>
2018-07-17 21:43:44 -0600

    Add feature test for user sees own reviews

---
+++ spec/features/reviews/user_can_see_reviews_they_wrote_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'user can see reviews they wrote' do
+  it 'can see reviews on user show page' do
+    user = User.create(username: 'Angela', password: '12345')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    author = Author.create(name: 'Card, Orson Scott')
+    book = author.books.create(title: "Ender's Game", year: 1985, image: "Ender's_game.jpg")
+    review1 = user.reviews.create(rating: 5, comment: 'Excellent!', book_id: book.id)
+    review2 = user.reviews.create(rating: 4, comment: 'Pretty Good!', book_id: book.id)
+
+    visit user_path(user)
+
+    expect(page).to have_content(review1.book.title)
+    expect(page).to have_content(review1.rating)
+    expect(page).to have_content(review1.comment)
+    expect(page).to have_content(review1.created_at)
+    expect(page).to have_content(review2.book.title)
+    expect(page).to have_content(review2.rating)
+    expect(page).to have_content(review2.comment)
+    expect(page).to have_content(review2.created_at)
+  end
+end
