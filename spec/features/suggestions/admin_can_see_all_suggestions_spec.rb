ec59be4b6ffb36c24dce4f015e29b7f0f3942642
Angela Duran <duranangela19@gmail.com>
2018-07-18 18:07:25 -0600

    Update some views, update suggestion index test for user

--- spec/features/suggestions/admin_can_see_all_suggestions_spec.rb
+++ spec/features/suggestions/admin_can_see_all_suggestions_spec.rb
@@ -20,4 +20,18 @@ describe 'only admin can see all suggesions' do
       expect(page).to have_content(suggestion.comment)
     end
   end
+  context 'as default user' do
+    it 'cannot see link or visit page' do
+      user = User.create(username: 'fred', password: '12345')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      suggestion = user.suggestions.create(author: 'Hesse', title: 'Siddhartha', rating: 4, comment: 'I liked it')
+
+      visit user_path(user)
+      expect(page).to_not have_content('See all suggestions')
+
+      visit suggestions_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
 end
